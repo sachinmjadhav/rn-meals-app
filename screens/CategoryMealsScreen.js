8ffe27e275fd1737e29567fee16f8a75303bb093
sachinmjadhav <sachin0321@gmail.com>
2019-09-04 12:10:30 +0530

    added dummy screens and fonts

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const CaregoryMealsScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Category Meal Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+});
+export default CaregoryMealsScreen;
---
+++ screens/FiltersScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FilterScreen = () => {
+  return (
+    <View style={styles.screen}>
+      <Text>The Filter Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default FilterScreen;
